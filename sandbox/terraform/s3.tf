22147f02e9ff43a7396393809da7f681085b90df
Prashant Mani Tripathi <prashant.tripathi@pathinfotech.com>
2022-07-25 13:48:52 +0530

    MY First aft commit

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,21 @@
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+#resource "aws_s3_bucket" "sandbox_bucket" {
+ # bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  #acl    = "private"
+#}
+
+
+resource "aws_s3_bucket" "AFT" {
+  bucket = "my-aft-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "AFT"
+  }
+}
+
+resource "aws_s3_bucket_acl" "AFT-bucket" {
+  bucket = aws_s3_bucket.AFT.id
   acl    = "private"
 }
